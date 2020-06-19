c4589377660fc3f9bf72e1d57a719e3a7c491472
kriss-19 <kris.sandwick@dat.com>
2020-06-19 15:30:24 -0700

    updated title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tourOfHeros'`, () => {
+  it(`should have as title 'Tour Of Heros'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Welcome to tourOfHeros');
+    expect(app.title).toEqual('Welcome to Tour Of Heros');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('tourOfHeros app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Tour Of Heros app is running!');
   });
 });
